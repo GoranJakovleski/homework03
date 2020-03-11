ca3eae4e82a8e946e73822f028e84cb467bc0e73
GoranJakovleski <zdravotoner@gmail.com>
2020-03-11 23:03:55 +0100

    Initial commit

---
+++ homework03/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace homework03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //***********************************EXERCISE 01********************************************
+
+            //Get an input number from the console
+            //Print all numbers from 1 to that number
+            //Get another input number from the console
+            //Print all numbers from that number to 1
+
+
+
+            //Console.WriteLine("Please enter some number");
+            //int converted;
+            //bool isConverted = int.TryParse(Console.ReadLine(), out converted);
+
+            //for (int i = 1; i <= converted; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Console.WriteLine("Please enter another number");
+            //int decrement;
+            //bool isdecrement = int.TryParse(Console.ReadLine(), out decrement);
+
+            //for (int n = decrement; n >= 1; n--)
+            //{
+            //    Console.WriteLine(n);
+            //}
+
+            //***********************************EXERCISE 02********************************************
+
+            //Get an input number from the console
+            //Print all even numbers starting from 2
+            //Get another input number from the console
+            //Print all odd numbers starting from 1
+
+
+            //Console.WriteLine("Please enter some number");
+            //int converted;
+            //bool isConverted = int.TryParse(Console.ReadLine(), out converted);
+
+            //for (int i = 2; i <= converted; i++)
+            //{
+            //    if (i % 2 == 0)
+            //        Console.WriteLine(i);
+            //}
+
+            //Console.WriteLine("Please enter another number");
+            //int decrement;
+            //bool isdecrement = int.TryParse(Console.ReadLine(), out decrement);
+
+            //for (int n = 1; n <= converted; n++)
+            //{
+            //    if (n % 2 != 0)         //ova so izvicnikot go pronajdov na google 
+            //        Console.WriteLine(n);
+            //}
+
+
+            //***********************************EXERCISE 03********************************************
+
+            //Create an array with names
+            //Give an option to the user to enter a name from the keyboard
+            //After entering the name put it in the array
+            //Ask the user if they want to enter another name(Y / N)
+            //Do the same process over and over until the user enters N
+            //Print all the names after user enters N
+            //Hint: we need to use endless loop until user enter N, then we want to "break" the loop
+
+
+                                   
+        }
+    }
+}
